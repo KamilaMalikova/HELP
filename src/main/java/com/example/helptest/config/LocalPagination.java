07b2f81204c5bbb02b00bc07d795ccdf6da68e08
KamilaMalikova <kamilamalikova173@gmail.com>
2020-10-10 17:37:32 +0500

    ...

--- src/main/java/com/example/helptest/config/LocalPagination.java
+++ src/main/java/com/example/helptest/config/LocalPagination.java
@@ -7,21 +7,21 @@ import org.springframework.data.domain.Sort;
 
 public class LocalPagination {
     public static Pageable getPageable(int page, String sortBy) {
-        page -= 1;
-        int total = 5;
+        //page -= 1;
+        int total = 20;
         Pageable pageable = PageRequest.of(page, total, Sort.by(sortBy));
         return pageable;
     }
 
     public static Pageable getDefaultPageable(int page) {
-        page -= 1;
+        //page -= 1;
         int total = 10;
         Pageable pageable = PageRequest.of(page, total, Sort.by("id"));
         return pageable;
     }
 
     public static Pageable getPageableWithTotal(int page, String sortBy, int total) {
-        page -= 1;
+        //page -= 1;
         Pageable pageable = PageRequest.of(page, total, Sort.by(sortBy));
         return pageable;
     }
